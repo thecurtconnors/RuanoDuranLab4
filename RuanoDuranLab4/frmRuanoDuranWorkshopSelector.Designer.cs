d00bae67c1b34c65575e8fa57fa858f09f390d0d
thecurtconnors <37684457+thecurtconnors@users.noreply.github.com>
2018-03-23 01:18:28 -0700

    Add project files.

---
+++ RuanoDuranLab4/frmRuanoDuranWorkshopSelector.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace RuanoDuranLab4
+{
+    partial class frmRuanoDuranWorkshopSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxWorkshopSelector = new System.Windows.Forms.GroupBox();
+            this.lblTotalCost = new System.Windows.Forms.Label();
+            this.lblLodgingCost = new System.Windows.Forms.Label();
+            this.lblRegistrationCost = new System.Windows.Forms.Label();
+            this.txtRegistrationCost = new System.Windows.Forms.TextBox();
+            this.txtTotalCost = new System.Windows.Forms.TextBox();
+            this.txtLodgingCost = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lstLocation = new System.Windows.Forms.ListBox();
+            this.lstWorkshop = new System.Windows.Forms.ListBox();
+            this.lblSelectWorkshop = new System.Windows.Forms.Label();
+            this.lblSelectCity = new System.Windows.Forms.Label();
+            this.gbxWorkshopSelector.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxWorkshopSelector
+            // 
+            this.gbxWorkshopSelector.Controls.Add(this.lblSelectCity);
+            this.gbxWorkshopSelector.Controls.Add(this.lblSelectWorkshop);
+            this.gbxWorkshopSelector.Controls.Add(this.lblTotalCost);
+            this.gbxWorkshopSelector.Controls.Add(this.lblLodgingCost);
+            this.gbxWorkshopSelector.Controls.Add(this.lblRegistrationCost);
+            this.gbxWorkshopSelector.Controls.Add(this.txtRegistrationCost);
+            this.gbxWorkshopSelector.Controls.Add(this.txtTotalCost);
+            this.gbxWorkshopSelector.Controls.Add(this.txtLodgingCost);
+            this.gbxWorkshopSelector.Controls.Add(this.btnExit);
+            this.gbxWorkshopSelector.Controls.Add(this.btnCalculate);
+            this.gbxWorkshopSelector.Controls.Add(this.lstLocation);
+            this.gbxWorkshopSelector.Controls.Add(this.lstWorkshop);
+            this.gbxWorkshopSelector.Location = new System.Drawing.Point(0, 0);
+            this.gbxWorkshopSelector.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxWorkshopSelector.Name = "gbxWorkshopSelector";
+            this.gbxWorkshopSelector.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxWorkshopSelector.Size = new System.Drawing.Size(981, 553);
+            this.gbxWorkshopSelector.TabIndex = 0;
+            this.gbxWorkshopSelector.TabStop = false;
+            // 
+            // lblTotalCost
+            // 
+            this.lblTotalCost.AutoSize = true;
+            this.lblTotalCost.Location = new System.Drawing.Point(580, 201);
+            this.lblTotalCost.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotalCost.Name = "lblTotalCost";
+            this.lblTotalCost.Size = new System.Drawing.Size(72, 17);
+            this.lblTotalCost.TabIndex = 10;
+            this.lblTotalCost.Text = "Total Cost";
+            // 
+            // lblLodgingCost
+            // 
+            this.lblLodgingCost.AutoSize = true;
+            this.lblLodgingCost.Location = new System.Drawing.Point(580, 169);
+            this.lblLodgingCost.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLodgingCost.Name = "lblLodgingCost";
+            this.lblLodgingCost.Size = new System.Drawing.Size(91, 17);
+            this.lblLodgingCost.TabIndex = 9;
+            this.lblLodgingCost.Text = "Lodging Cost";
+            // 
+            // lblRegistrationCost
+            // 
+            this.lblRegistrationCost.AutoSize = true;
+            this.lblRegistrationCost.Location = new System.Drawing.Point(580, 137);
+            this.lblRegistrationCost.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRegistrationCost.Name = "lblRegistrationCost";
+            this.lblRegistrationCost.Size = new System.Drawing.Size(116, 17);
+            this.lblRegistrationCost.TabIndex = 8;
+            this.lblRegistrationCost.Text = "Registration Cost";
+            // 
+            // txtRegistrationCost
+            // 
+            this.txtRegistrationCost.Location = new System.Drawing.Point(704, 133);
+            this.txtRegistrationCost.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtRegistrationCost.Name = "txtRegistrationCost";
+            this.txtRegistrationCost.ReadOnly = true;
+            this.txtRegistrationCost.Size = new System.Drawing.Size(132, 22);
+            this.txtRegistrationCost.TabIndex = 7;
+            // 
+            // txtTotalCost
+            // 
+            this.txtTotalCost.Location = new System.Drawing.Point(704, 197);
+            this.txtTotalCost.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTotalCost.Name = "txtTotalCost";
+            this.txtTotalCost.ReadOnly = true;
+            this.txtTotalCost.Size = new System.Drawing.Size(132, 22);
+            this.txtTotalCost.TabIndex = 6;
+            // 
+            // txtLodgingCost
+            // 
+            this.txtLodgingCost.Location = new System.Drawing.Point(704, 165);
+            this.txtLodgingCost.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtLodgingCost.Name = "txtLodgingCost";
+            this.txtLodgingCost.ReadOnly = true;
+            this.txtLodgingCost.Size = new System.Drawing.Size(132, 22);
+            this.txtLodgingCost.TabIndex = 5;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(352, 334);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(160, 28);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(352, 284);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(160, 28);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate Total";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lstLocation
+            // 
+            this.lstLocation.FormattingEnabled = true;
+            this.lstLocation.ItemHeight = 16;
+            this.lstLocation.Items.AddRange(new object[] {
+            "Austin",
+            "Chicago",
+            "Dallas",
+            "Orlando",
+            "Phoenix",
+            "Raleigh"});
+            this.lstLocation.Location = new System.Drawing.Point(352, 137);
+            this.lstLocation.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstLocation.Name = "lstLocation";
+            this.lstLocation.Size = new System.Drawing.Size(159, 116);
+            this.lstLocation.TabIndex = 1;
+            // 
+            // lstWorkshop
+            // 
+            this.lstWorkshop.FormattingEnabled = true;
+            this.lstWorkshop.ItemHeight = 16;
+            this.lstWorkshop.Items.AddRange(new object[] {
+            "Handling Stress",
+            "Time Management",
+            "Supervision Skills",
+            "Negotiation",
+            "How to Interview"});
+            this.lstWorkshop.Location = new System.Drawing.Point(125, 137);
+            this.lstWorkshop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstWorkshop.Name = "lstWorkshop";
+            this.lstWorkshop.Size = new System.Drawing.Size(159, 116);
+            this.lstWorkshop.TabIndex = 0;
+            // 
+            // lblSelectWorkshop
+            // 
+            this.lblSelectWorkshop.AutoSize = true;
+            this.lblSelectWorkshop.Location = new System.Drawing.Point(122, 102);
+            this.lblSelectWorkshop.Name = "lblSelectWorkshop";
+            this.lblSelectWorkshop.Size = new System.Drawing.Size(119, 17);
+            this.lblSelectWorkshop.TabIndex = 11;
+            this.lblSelectWorkshop.Text = "Select Workshop:";
+            // 
+            // lblSelectCity
+            // 
+            this.lblSelectCity.AutoSize = true;
+            this.lblSelectCity.Location = new System.Drawing.Point(349, 102);
+            this.lblSelectCity.Name = "lblSelectCity";
+            this.lblSelectCity.Size = new System.Drawing.Size(78, 17);
+            this.lblSelectCity.TabIndex = 12;
+            this.lblSelectCity.Text = "Select City:";
+            // 
+            // frmRuanoDuranWorkshopSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1441, 800);
+            this.Controls.Add(this.gbxWorkshopSelector);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmRuanoDuranWorkshopSelector";
+            this.Text = "Workshop Selector";
+            this.gbxWorkshopSelector.ResumeLayout(false);
+            this.gbxWorkshopSelector.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxWorkshopSelector;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lstLocation;
+        private System.Windows.Forms.ListBox lstWorkshop;
+        private System.Windows.Forms.TextBox txtTotalCost;
+        private System.Windows.Forms.TextBox txtLodgingCost;
+        private System.Windows.Forms.Label lblTotalCost;
+        private System.Windows.Forms.Label lblLodgingCost;
+        private System.Windows.Forms.Label lblRegistrationCost;
+        private System.Windows.Forms.TextBox txtRegistrationCost;
+        private System.Windows.Forms.Label lblSelectCity;
+        private System.Windows.Forms.Label lblSelectWorkshop;
+    }
+}
+
---
+++ RuanoDuranLab4/frmRuanoDuranWorkshopSelector.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RuanoDuranLab4
+{
+    public partial class frmRuanoDuranWorkshopSelector : Form
+    {
+        public frmRuanoDuranWorkshopSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye!");
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //declaring variables
+            string workshop, city;
+            decimal workshopCost, lodgingCost, totalCost;
+            int days;
+
+            //declare constants for workshop prices & lodging base pricing                
+            const decimal HandlingStress = 1000, TimeManagement = 800, SupervisionSkills = 1500,
+            Negotiation = 1300, HowToInterview = 500, AustinLodging = 150, ChicagoLodging = 225,
+            DallasLodging = 175, OrlandoLodging = 300, PhoenixLodging = 175, RaleighLodging = 150;
+
+
+
+            if (lstWorkshop.SelectedIndex != -1)
+
+            {
+                workshop = lstWorkshop.SelectedItem.ToString();
+
+                //setup price for each workshop
+                switch (workshop)
+
+                {
+                    case "Handling Stress":
+                        txtRegistrationCost.Text = HandlingStress.ToString("c");
+                        workshopCost = 1000m;
+                        days = 3;
+                        break;
+                    case "Time Management":
+                        txtRegistrationCost.Text = TimeManagement.ToString("c");
+                        workshopCost = 800m;
+                        days = 3;
+                        break;
+                    case "Supervision Skills":
+                        txtRegistrationCost.Text = SupervisionSkills.ToString("c");
+                        workshopCost = 1500m;
+                        days = 3;
+                        break;
+                    case "Negotiation":
+                        txtRegistrationCost.Text = Negotiation.ToString("c");
+                        workshopCost = 1300;
+                        days = 5;
+                        break;
+                    case "How to Interview":
+                        txtRegistrationCost.Text = HowToInterview.ToString("c");
+                        workshopCost = 500m;
+                        days = 1;
+                        break;
+                }
+            }
+
+            else
+
+            {
+                //No Workshop Selected :(
+                MessageBox.Show("Select a Workshop!");
+            }
+
+            if (lstLocation.SelectedIndex != -1)
+            {
+
+                city = lstLocation.SelectedItem.ToString();
+
+                //setup price for each city
+                switch (city)
+                {
+                    case "Austin":
+                        txtLodgingCost.Text = AustinLodging.ToString("c");
+                        lodgingCost = 150;
+                        break;
+                    case "Chicago":
+                        txtLodgingCost.Text = ChicagoLodging.ToString("c");
+                        lodgingCost = 225;
+                        break;
+                    case "Dallas":
+                        txtLodgingCost.Text = DallasLodging.ToString("c");
+                        lodgingCost = 175;
+                        break;
+                    case "Orlando":
+                        txtLodgingCost.Text = OrlandoLodging.ToString("c");
+                        lodgingCost = 300;
+                        break;
+                    case "Phoenix":
+                        txtLodgingCost.Text = PhoenixLodging.ToString("c");
+                        lodgingCost = 175;
+                        break;
+                    case "Raleigh":
+                        txtLodgingCost.Text = RaleighLodging.ToString("c");
+                        lodgingCost = 150;
+                        break;
+                }
+            }
+            else
+            {
+                //No City Selected :(
+                MessageBox.Show("Select a City!");
+            }
+
+
+      
+            {
+
+
+
+            //calculate totals
+
+                if (decimal.TryParse(txtRegistrationCost.Text, out workshopCost))
+
+                {
+                    if (decimal.TryParse(txtLodgingCost.Text, out lodgingCost))
+                    {
+
+
+                        totalCost = lodgingCost + workshopCost;
+
+                        txtTotalCost.Text = totalCost.ToString("c");
+
+
+                    }
+                    else
+
+                    {
+
+                    }
+
+                }
+
+            else
+                {
+
+                }
+
+
+            }
+
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
